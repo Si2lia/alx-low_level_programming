a6a843ed5a1d422c59c16f084add2b0c7ca57810
Si2lia <soukaina.mourisse02@gmail.com>
2023-03-19 19:48:33 -0700

    we print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,20 +8,20 @@
  */
 int main(void)
 {
-        int n;
-        int Last;
-        char str[] = "Last digit of";
+	int n;
+	int L;
+	char str[] = "Last digit of";
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        Last = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	L = n % 10;
 
-        if (Last > 5)
-                printf("%s %d is %d and is greater than 5\n", str, n, Last);
-        else if (Last == 0)
-                printf("%s %d is %d and is 0\n", str, n, Last);
-        else if (Last < 6)
-                printf("%s %d is %d and is less than 6 and not 0\n", str, n, Last);
+	if (L > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+		printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 
-        return (0);
+	return (0);
 }
